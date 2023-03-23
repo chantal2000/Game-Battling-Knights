a3b208fca42182a5375a2f2cce9ededf571caa35
chantal2000 <nniyonkuruchantal@gmail.com>
2023-03-23 14:33:24 +0200

    Battling-Knights

--- item.py
+++ item.py
@@ -1,9 +1,11 @@
 from dataclasses import dataclass
 from pos import Pos
+
 @dataclass
 class Item:
     name: str
-    priority: int 
-    pos: Pos
     attack: int
     defence: int
+    priority: int  
+    pos: Pos
+   
